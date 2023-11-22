48b24d5d9f408a2607327dd580016670ac4ff808
Anthony Hitalessij <a.s.f.hitalessij@students.uu.nl>
2023-11-22 11:41:46 +0100

    Added template

---
+++ YourEngine/Camera.cs
@@ -0,0 +1,19 @@
+﻿namespace YourEngine
+{
+    /// <summary>
+    /// You will have to program this yourself if you want a camera.
+    /// Just know that GameObject.cs and Sprite.cs are already designed
+    /// to take scrolling into account (study the code)!
+    /// </summary>
+    public class Camera // Hmm... does it have to inherit from GameObject or not (genuine question)?
+    {
+        public Camera() : base()
+        {
+        }
+
+        // Hmm... static or not static? And why (most important)? !PERSONAL OPINION!: not static ;-)!
+        // Hmm... how will the camera affect all GameObject instances in your game?
+        // Hmm... so much to think about.
+        // Designing programs is so fun (genuinely)!
+    }
+}
---
+++ YourGame/Objects/Player.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using YourEngine;
+
+namespace YourGame.Objects
+{
+    /// <summary>
+    /// Just know that the player looks like a tree in this example.
+    /// </summary>
+    public sealed class Player : GameObject
+    {
+        public Player() : base()
+        {
+            this.Velocity = 64;
+
+            this.Sprite = new Sprite(YourGame.AssetManager.LoadTexture("TX Plant", "Pixel Art Top Down - Basic/"))
+            {
+                SourceRectangle = new Rectangle(location: Point.Zero, size: new Point(160, 153)),
+                OriginType = OriginType.BottomCenter,
+            };
+
+            this.AddChild(this.Sprite);
+        }
+
+        public Sprite Sprite { get; }
+
+        protected override void EnterSelf()
+        {
+            //
+        }
+
+        protected override void UpdateSelf(GameTime gameTime)
+        {
+            // Update direction (standard 8-way movement code).
+            Vector2 direction = Vector2.Zero;
+
+            if (YourGame.InputManager.CheckIsKeyPressed(Keys.W))
+                direction -= Vector2.UnitY;
+
+            if (YourGame.InputManager.CheckIsKeyPressed(Keys.A))
+                direction -= Vector2.UnitX;
+
+            if (YourGame.InputManager.CheckIsKeyPressed(Keys.S))
+                direction += Vector2.UnitY;
+
+            if (YourGame.InputManager.CheckIsKeyPressed(Keys.D))
+                direction += Vector2.UnitX;
+
+            this.Direction = direction;
+
+            // Update angle.
+            this.Sprite.AngleDegrees = YourMath.Sine(
+                elapsedTime: gameTime.TotalGameTimeInSeconds(),
+                amplitude: 11.25f,
+                period: 3
+                );
+
+            // Update scale.
+            this.Sprite.Scale = new Vector2(YourMath.Sine(
+                elapsedTime: gameTime.TotalGameTimeInSeconds(),
+                amplitude: 1.25f,
+                period: 3
+                ).Clamp(1, 1.25f));
+        }
+
+        protected override void DrawSelf(SpriteBatch spriteBatch)
+        {
+            //
+        }
+
+        protected override void ExitSelf()
+        {
+            //
+        }
+    }
+}
